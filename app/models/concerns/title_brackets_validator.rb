c04f867125d1bd9c44192e661380df9ca047ca66
Paweł Potasiewicz <potasiewicz@gmail.com>
2018-11-07 18:53:48 +0100

    information about too much opening or closing brackets

--- app/models/concerns/title_brackets_validator.rb
+++ app/models/concerns/title_brackets_validator.rb
@@ -1,5 +1,19 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(obj)
+    chars = obj.title.split(//)
 
+    brackets = {"[" => "]", "{" => "}", "(" => ")"}
+    opening_brackets = []
+    closing_brackets = []
+    chars.each_with_index do |char, idx|
+      if brackets.keys.include?(char)
+        opening_brackets.push(char)
+      elsif brackets.values.include?(char)
+        closing_brackets.push(char)
+      end
+    end
+
+    return obj.errors[:base] << 'too much closing brackets' if opening_brackets.size < closing_brackets.size
+    return obj.errors[:base] << 'too much opeing brackets' if closing_brackets.size < opening_brackets.size
   end
 end
