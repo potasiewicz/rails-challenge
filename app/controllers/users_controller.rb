2f70a902a47df2fe84d0ba10859da0b29673dd1e
Paweł Potasiewicz <potasiewicz@gmail.com>
2018-11-11 14:02:29 +0100

    top commenters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def commenters
-    @users = User.includes(:comments)
+    @commenters = Comment.includes(:user)
+                      .joins(:user)
+                      .where({created_at: (DateTime.now - 7.days)..DateTime.now})
+                      .group('users.name')
+                      .limit(10)
+                      .count
+                      .sort_by {|name, comments| comments}
+                      .reverse
   end
 end
