6beab661402348f2ec70391c0fbcea01e33271a7
Paweł Potasiewicz <potasiewicz@gmail.com>
2018-11-11 15:17:22 +0100

    refactroing in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
   def commenters
-    @commenters = Comment.includes(:user)
-                      .joins(:user)
-                      .where({created_at: (DateTime.now - 7.days)..DateTime.now})
-                      .group('users.name')
-                      .limit(10)
-                      .count
-                      .sort_by {|name, comments| comments}
-                      .reverse
+    @commenters = UserService.new.top_commenters
   end
 end
